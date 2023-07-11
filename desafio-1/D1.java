ffa0de506f38ac1928ba768fa42d7700cda8bea5
Otávio <otavio.rodf@gmail.com>
2023-07-11 16:27:55 -0300

    resoled desafio 1 & desafio 2

--- desafio-1/D1.java
+++ desafio-1/D1.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Comparator;
+
 public class D1{
   public static void main(String [] args){
-    System.out.println("Teste");
+    Scanner sc = new Scanner(System.in);
+    int linhas = sc.nextInt();
+
+    int nums;
+    ArrayList<Integer> pares = new ArrayList<Integer>();
+    ArrayList<Integer> impares = new ArrayList<Integer>();
+    ArrayList<Integer> total = new ArrayList<Integer>();
+    
+    for (int i = 0; i < linhas; i++){
+      nums = sc.nextInt();
+      (nums % 2 == 0 ? pares : impares).add(nums);
+    }
+    
+    pares.sort(Comparator.naturalOrder());
+    impares.sort(Comparator.reverseOrder());
+
+    total.addAll(pares);
+    total.addAll(impares);
+
+    for (int numero: total){
+      System.out.println(numero);
+    }
+
+    sc.close();
   }
 }
--- desafio-2/D2.java
+++ desafio-2/D2.java
@@ -1,5 +1,33 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class D2{
   public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+    BigDecimal entrada = sc.nextBigDecimal(); 
+    
+    int notas[] = {100, 50, 20, 10, 5, 2};
+    BigDecimal moedas[] = {BigDecimal.ONE, new BigDecimal("0.50"), new BigDecimal("0.25"),
+    new BigDecimal("0.10"), new BigDecimal("0.05"), new BigDecimal("0.01")};
+    
+    System.out.println("NOTAS:");
+    for (int nota: notas){
+      var numNotas = entrada.divideToIntegralValue(new BigDecimal(nota)).intValue();
+      System.out.println(
+         numNotas + "    nota(s)    de    R$    " + nota
+      );
+      entrada = entrada.remainder(new BigDecimal(nota));
+    }
 
+    System.out.println("MOEDAS:");
+    for (BigDecimal moeda: moedas){
+      var numMoedas = entrada.divideToIntegralValue(moeda).intValue();
+      System.out.printf(
+        numMoedas + "    moeda(s)    de    R$    %.2f%n", moeda
+      );
+      entrada = entrada.remainder(moeda);
+    }
+
+    sc.close();
   }
 }
