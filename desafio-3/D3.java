fc262094315775eb21301c3f15cfe763b4809c23
Otávio <otavio.rodf@gmail.com>
2023-07-11 22:31:22 -0300

    resolved desafio 3

--- desafio-3/D3.java
+++ desafio-3/D3.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class D3{
   public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
 
+    int tamanhoArray = sc.nextInt();
+    int alvo = sc.nextInt();
+
+    int array[] = new int[tamanhoArray];
+
+    for (int i = 0; i < tamanhoArray; i++){
+      array[i] = sc.nextInt();
+    }
+
+    int cont = 0;
+    
+    for(int i = 0; i < array.length; i++) {
+      for(int k = i + 1; k < array.length; k++){
+        if (Math.abs(array[i] - array[k]) == alvo) cont++;
+      }
+    }
+
+    System.out.println(cont);
+
+    sc.close();
   }
 }
