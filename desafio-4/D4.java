4fccaca06f616cafda02ac753b4c981482062ef3
Otávio <otavio.rodf@gmail.com>
2023-07-11 23:05:12 -0300

    resolved desafio 4

--- desafio-4/D4.java
+++ desafio-4/D4.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class D4{
   public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+    int entrada = sc.nextInt();
+    sc.nextLine();
+    ArrayList<String> frases = new ArrayList<>();
 
+    for (int i = 0; i < entrada; i++){
+      String frase = sc.nextLine();
+      StringBuilder inicio = new StringBuilder(frase.substring(0, frase.length()/ 2));
+      StringBuilder fim = new StringBuilder(frase.substring(frase.length()/ 2, frase.length()));
+      frases.add(frase = inicio.reverse().toString() + fim.reverse().toString());
+    }
+
+    for(String f: frases) System.out.println(f);
+    sc.close();
   }
 }
